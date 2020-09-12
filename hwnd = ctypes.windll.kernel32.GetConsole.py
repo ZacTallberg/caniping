22ac0a6b61c7290abfccf3823e7a007ceceec7c6
unknown <zcoberg@gmail.com>
2020-09-11 17:21:44 -0700

    cleanup

--- hwnd = ctypes.windll.kernel32.GetConsole.py
+++
@@ -1,6 +0,0 @@
-hwnd = ctypes.windll.kernel32.GetConsoleWindow()
-    if hwnd != 0:
-        ctypes.windll.user32.ShowWindow(hwnd, 0)      
-        ctypes.windll.kernel32.CloseHandle(hwnd)
-        _, pid = win32process.GetWindowThreadProcessId(hwnd)
-        os.system('taskkill /PID ' + str(pid) + ' /f')
